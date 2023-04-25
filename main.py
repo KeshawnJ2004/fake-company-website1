ce2ab14c62e4eb07ae9ba7e3f871925294ace3f7
Initial Commit | cmd line to-do app <keshawn936@gmail.com>
2023-04-25 17:43:17 -0400

    Initial commit + complete first column

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas
+import streamlit as stream
+
+df = pandas.read_csv('team_info.csv', sep=',')
+stream.header('The best company')
+content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' \
+          ', sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' \
+          ' Proin tortor purus platea sit eu id nisi litora libero.' \
+          'Neque vulputate consequat ac amet augue blandit maximus' \
+          ' aliquet congue. Pharetra vestibulum posuere ornare ' \
+          'faucibus fusce dictumst orci aenean eu facilisis ut' \
+          ' volutpat commodo senectus purus himenaeos famesprimis convallis nisi.'
+stream.write(content)
+stream.subheader('Our team')
+col1, col2, col3 = stream.columns(3)
+
+with col1:
+    for index, rows in df[:4].iterrows():
+        stream.subheader((f"{rows['first name']} {rows['last name']}"))
+        stream.write(rows['role'])
+        stream.image(f"team_portraits/{rows['image']}")
+
+
